a7321e001f227968bead78a9af6ee19761face40
Rima Mondal <rima.mondal@wealthtech.in>
2022-06-26 01:45:09 +0530

    add employee navigation

---
+++ src/app/employee-list/employee-list.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { Employee } from '../employee';
+import { EmployeeService } from '../employee.service';
+
+@Component({
+  selector: 'app-employee-list',
+  templateUrl: './employee-list.component.html',
+  styleUrls: ['./employee-list.component.css']
+})
+export class EmployeeListComponent implements OnInit {
+
+  employees : Employee[] | undefined;
+  constructor(private employeeService:EmployeeService) { }
+
+  ngOnInit(): void {
+   this.getEmployee();
+  }
+
+getEmployee(){
+  this.employeeService.getEmployeeList().subscribe(data=>{
+    this.employees=data;
+  })
+}
+
+
+
+}
